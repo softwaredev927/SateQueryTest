da6ec3fc66dbd2b34391b0ec2157a901a3964173
Clement Lucas <lclement927@gmail.com>
2021-08-12 12:40:48 -0400

    Integrate thegraph to query sate

--- routes/sate.js
+++ routes/sate.js
@@ -1,23 +1,48 @@
+const { createClient } = require('urql')
+const APIURL = "https://gateway.thegraph.com/api/dcdad49933b01cb3b64b3b366cbf6865/subgraphs/id/0x7034196e9d67ac9fa4a736da1be9e99127a1124b-0"
+const sateQuery = `
+  query($tokenId:String) {
+    sate(id:$tokenId) {
+        id
+        launchTime
+        launchPrice
+        apr
+    }
+  }
+`
+const client = createClient({
+    url: APIURL
+})
+global.fetch = require("node-fetch")
 const model = require('../model')
-const {sateNftContract} = require('../contracts')
 
 const SatePage = async (req,res) => {
     let tokenId = req.query.tokenId
     if(tokenId){
-        const sate = await sateNftContract.methods.sateInfo(parseInt(tokenId)).call()
-        const sateObj = {
-            st_id: tokenId,
-            st_launchPrice: sate.st_launchPrice,
-            st_launchTime: new Date(parseInt(sate.st_launchTime) * 1000),
-            st_apr: parseInt(sate.st_apr)
+        const queryRes = await client.query(sateQuery, {tokenId: tokenId}).toPromise()
+        if (queryRes && queryRes.data && queryRes.data.sate) {                
+            const sate = queryRes.data.sate
+            const sateObj = {
+                st_id: tokenId,
+                st_launchPrice: sate.launchPrice,
+                st_launchTime: new Date(parseInt(sate.launchTime) * 1000),
+                st_apr: parseInt(sate.apr)
+            }
+            try {
+                await model.sate.add(sateObj)
+                const sateRes = await model.sate.get(tokenId)
+                res.render("sate", sateRes.length > 0 ? sateRes[0] : {})
+            } catch(ex) {
+                const sateRes = await model.sate.get(tokenId)
+                if (sateRes.length > 0) {
+                    res.render("sate", sateRes[0])
+                } else {
+                    res.render("error", {error_message: "A sate with the same launchTime or id is already exist."})
+                }
+            }
+        } else {
+            res.render("error", {error_message: "Sate with this tokenId isn't exist."})
         }
-        try {
-            await model.sate.add(sateObj)
-        } catch(ex) {
-            console.log("already exist")
-        }
-        const sateRes = await model.sate.get(tokenId)
-        res.render("sate", sateRes[0])
     }else{
         res.render("error")
     }
